31aefd1d17b4686fad5037bbd7307395017287b2
Mateusz Naklicki <mateusz.naklicki06@gmail.com>
2022-12-06 21:37:38 +0100

    Add basic backend code

---
+++ air-quality-backend/server.py
@@ -0,0 +1,32 @@
+from flask import Flask, Response, jsonify, request
+from waitress import serve
+
+ACCEPTED_DATA_REQUESTS = ["pm10", "no2", "co"] 
+
+app = Flask("air_quality_backend")
+
+@app.route("/")
+def hello_world():
+    return "This is API server for retrieving data. Please use /api/..."
+
+@app.route('/api/<path:data_name>')
+def data_request(data_name, methods=['GET, POST']):
+    data_name = data_name.lower()
+    data = get_data(data_name)
+    if (data):
+        return jsonify(data)
+    else:
+        response = jsonify(f"Request for {data_name} not supported")
+        response.status_code = 400
+        return response
+
+def get_data(data_name):
+    if (data_name not in ACCEPTED_DATA_REQUESTS):
+        return False
+    if (data_name == "co"):
+        pass
+
+    return False
+
+if __name__ == "__main__":
+    serve(app, host="localhost", port=3001)
