c915a634beffd17f0e1993fe2d94b346716d0321
mrochny <tpalinski@icloud.com>
2022-12-07 00:54:53 +0100

    Add loading indicator

--- air-quality-app/client/src/App.tsx
+++ air-quality-app/client/src/App.tsx
@@ -7,12 +7,16 @@ import { getData } from "./api/api";
 function App() {
   let [graphData, setGraphData] = useState<GraphResponseData>();
   let [timePeriod, setTimePeriod] = useState(0);
+  let [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    setIsLoading(true);
     getData('timePeriod').then((response) => {
       console.log(response)
       setGraphData(response)
+      setIsLoading(false);
     })
+    
   }, [timePeriod])
 
   const handlePeriodChange = (e: any) => {
@@ -29,7 +33,7 @@ function App() {
           <p>Lorem ipsum i takie tam</p>
         </div>
         <div className='Graphs'>
-          <button onClick={handlePeriodChange}> Fetch Data</button>
+          {isLoading ? <div className='Loader'></div> : <button onClick={handlePeriodChange} disabled={isLoading}> Fetch Data</button>}
           <GraphsContainer text="CO" data={graphData?.co}/>
           <GraphsContainer text='NO2' data={graphData?.no2}/>
           <GraphsContainer text='PM10' data={graphData?.pa}/>
