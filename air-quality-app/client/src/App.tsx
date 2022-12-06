4be4b32f000229369b4605bdfec8f371fd8901ef
mrochny <trollklop@gmail.com>
2022-12-06 16:53:02 +0100

    Add simple landing page

--- air-quality-app/client/src/App.tsx
+++ air-quality-app/client/src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='Title'>
+        <h1>Air Quality - Gdańsk</h1>
+      </div>
+      <div className='Content'>
+        <div className='Lore'>
+          <h3> Tytul tego wszystkiego</h3>
+          <p>Lorem ipsum i takie tam</p>
+        </div>
+      </div>
+      
     </div>
   );
 }
