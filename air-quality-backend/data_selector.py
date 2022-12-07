76f5881c02b6c00dc2cc8af7ff5c57685cf693d1
Mateusz Naklicki <mateusz.naklicki06@gmail.com>
2022-12-07 01:16:35 +0100

    Select data by station instead of pollution type

--- air-quality-backend/data_selector.py
+++ air-quality-backend/data_selector.py
@@ -20,26 +20,48 @@ class DataSelector:
         except FileNotFoundError:
             print("ERROR: Failed to load data files!")
 
-    # data_name: "pm10"/"no2"/"co"
+    # station: PmGdaLeczkow/PmGdaPowWars/PmGdaWyzwole/PmGdyPorebsk/PmGdySzafran/PmSopBiPlowoc
     # date_format: RRRR-MM-DD HH:MM:SS
-    def select_by_date(self, data_name, start_date, end_date):
+    def select_by_date(self, station, start_date, end_date):
         if (valid_pd_date(start_date) == False) or \
-        (valid_pd_date(end_date) == False) or \
-        (data_name not in self.datasets):
+           (valid_pd_date(end_date) == False):
             return ""
-        dataset = self.datasets[data_name]
+        
+        # selecting by station
+        dataset = self.datasets["co"][["data",station]]
+        
+        # selecting by date range
         date_mask = (dataset["data"] >= start_date) & (dataset["data"] <= end_date)
         selected_data = dataset.loc[date_mask]
-        return selected_data.to_json()
+
+        # formatting data
+        selected_data["data"] = selected_data["data"].dt.strftime("%Y-%m-%d %H:%M")
+        
+        # create and format json to frontend requirements
+        json = selected_data.to_json(orient="values")
+        json = json.replace("[", "{").replace("]", "}")
+        json = "[" + json[1:-1] + "]"
+
+        return json
 
 
 if __name__ == "__main__":
-    # EXAMPLE OF SELECTING RANGE
+    # EXAMPLE OF SELECTING DATA FROM DATE RANGE
     dataset_co = pd.read_csv("data/co.csv", parse_dates=["data"])
     start_date = "2021-02-02 10:00:00"
-    end_date = "2021-02-02 20:00:00"
+    end_date = "2021-02-02 11:00:00"
     mask = (dataset_co["data"] >= start_date) & (dataset_co["data"] <= end_date)
 
     selected_dataset_co = dataset_co.loc[mask]
+    selected_dataset_co = selected_dataset_co[["data","PmGdaLeczkow"]]
     print(selected_dataset_co)
     print(dataset_co.dtypes)
+
+    selected_dataset_co["data"] = selected_dataset_co["data"].dt.strftime("%Y-%m-%d %H:%M")
+    json = selected_dataset_co.to_json(orient="values")
+
+    # format json to frontend requirements
+    json = json.replace("[", "{").replace("]", "}")
+    json = "[" + json[1:-1] + "]"
+
+    print(json)
--- air-quality-backend/server.py
+++ air-quality-backend/server.py
@@ -2,7 +2,7 @@ from flask import Flask, Response, jsonify, request
 from waitress import serve
 from data_selector import DataSelector
 
-ACCEPTED_DATA_REQUESTS = ["pm10", "no2", "co"] 
+ACCEPTED_STATION_REQUESTS = ["PmGdaLeczkow", "PmGdaPowWars", "PmGdaWyzwole", "PmGdyPorebsk", "PmGdySzafran", "PmSopBiPlowoc"]
 
 app = Flask("air_quality_backend")
 data_selector = DataSelector()
@@ -11,14 +11,13 @@ data_selector = DataSelector()
 def hello_world():
     return "This is API server for retrieving data. Please use /api/..."
 
-@app.route('/api/<path:data_name>')
-def data_request(data_name, methods=['GET, POST']):
-    data_name = data_name.lower()
-    data = get_data(data_name)
+@app.route('/api/<path:station_name>')
+def data_request(station_name, methods=['GET, POST']):
+    data = get_data(station_name)
     if (data):
         return jsonify(data)
     else:
-        response = jsonify(f"Request for {data_name} not supported")
+        response = jsonify(f"Request for {station_name} not supported")
         response.status_code = 400
         return response
 
@@ -26,10 +25,10 @@ def data_request(data_name, methods=['GET, POST']):
 def page_not_found(error):
     return hello_world()
 
-def get_data(data_name):
-    if (data_name not in ACCEPTED_DATA_REQUESTS):
+def get_data(station_name):
+    if (station_name not in ACCEPTED_STATION_REQUESTS):
         return False
-    result = data_selector.select_by_date(data_name, "2021-02-02 10:00:00", "2021-02-02 11:00:00")
+    result = data_selector.select_by_date(station_name, "2021-02-02 10:00:00", "2021-02-02 11:00:00")
 
     return result
 
