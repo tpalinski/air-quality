f27cdd72430e85c828fd2827ca293fd12eb4b544
mrochny <tpalinski@icloud.com>
2022-12-08 21:21:10 +0100

    Add map data selection and mock data fetching

--- air-quality-app/client/src/App.tsx
+++ air-quality-app/client/src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 import { GraphsContainer } from "./components/GraphsContainer";
 import { PeriodPicker } from "./components/PeriodPicker";
 import { GraphResponseData, TimePeriod } from "./types";
-import { getData, getDataDebug } from "./api/api";
+import { getData} from "./api/api";
 import { MapSelector } from './components/MapSelector';
 
 function App() {
@@ -52,26 +52,25 @@ function App() {
         </div>
 		
         <MapSelector />
-		<div className='Conclusions'>
-			Ziemia i środowisko zmagają się z poważnym problemem zwiększającego się zanieczyszczenia powietrza, wody oraz gleby.  Zanieczyszczenia te są spowodowane w większości działalnością człowieka i mogą mieć fatalne skutki dla flory, fauny i ludzi. Jednymi z najgroźniejszych cząsteczek są tlenki węgla, dwutlenki azotu oraz różne inne cząsteczki zawieszone w powietrzu o średnicy nie większej niż 10 mikrometrów (określane nazwą PM10), pochodzące najczęściej z spalania paliw stałych i ciekłych. <br></br><br></br>
+        <div className='Conclusions'>
+          Ziemia i środowisko zmagają się z poważnym problemem zwiększającego się zanieczyszczenia powietrza, wody oraz gleby.  Zanieczyszczenia te są spowodowane w większości działalnością człowieka i mogą mieć fatalne skutki dla flory, fauny i ludzi. Jednymi z najgroźniejszych cząsteczek są tlenki węgla, dwutlenki azotu oraz różne inne cząsteczki zawieszone w powietrzu o średnicy nie większej niż 10 mikrometrów (określane nazwą PM10), pochodzące najczęściej z spalania paliw stałych i ciekłych. <br></br><br></br>
 
-			Jednak poziom zanieczyszczenia powietrza nie jest związany wyłącznie z działalnością człowieka. Ma na niego wpływ wielu czynników takich jak warunki pogodowe. W naszych obliczeniach skupiliśmy się na zależności pomiędzy temperaturą powietrza, prędkością wiatru oraz zachmurzeniem, a jakością powietrza. Nasze pomiary pochodzą z placówki Instytutu Meteorologii i Gospodarki Wodnej w Gdańsku oraz siedmiu placówek dokonujących pomiaru zanieczyszczeń powietrza na terenie Trójmiasta. <br></br><br></br>
+          Jednak poziom zanieczyszczenia powietrza nie jest związany wyłącznie z działalnością człowieka. Ma na niego wpływ wielu czynników takich jak warunki pogodowe. W naszych obliczeniach skupiliśmy się na zależności pomiędzy temperaturą powietrza, prędkością wiatru oraz zachmurzeniem, a jakością powietrza. Nasze pomiary pochodzą z placówki Instytutu Meteorologii i Gospodarki Wodnej w Gdańsku oraz siedmiu placówek dokonujących pomiaru zanieczyszczeń powietrza na terenie Trójmiasta. <br></br><br></br>
 
-			--------wykresy i zdjęcia---------------
-			
-			<br></br><br></br>
-			Jak można zauważyć, największa korelacja występuje pomiędzy temperaturą a zawartością tlenku węgla oraz dwutlenku azotu. Istnieje również zależność między temperaturą a PM10, jednak nie jest tak znacząca. Im temperatura jest niższa, tym powietrze staje się bardziej zanieczyszczone.
-			Połączenie to jest spowodowane spalaniem większej ilości paliw w celu ogrzania mieszkań, biur czy fabryk.
-			<br></br><br></br>
-			
-			Równie znaczącą relacją jest to, że wszystkie zanieczyszczenia występują odwrotnie proporcjonalnie do prędkości wiatru, który im z większą prędkością wieje, tym bardziej rozwiewa zawieszone w nim cząsteczki.
-			<br></br>
-			Nasze obliczenia wskazały brak jednoznacznego powiązania między obecnością szkodliwych substancji w powietrzu a zachmurzeniem.
-			<br></br><br></br>
-			Podsumowując najlepszą porą do spędzania czasu na dworze są ciepłe i wietrzne dni. :)
-		</div>
+          --------wykresy i zdjęcia---------------
+          
+          <br></br><br></br>
+          Jak można zauważyć, największa korelacja występuje pomiędzy temperaturą a zawartością tlenku węgla oraz dwutlenku azotu. Istnieje również zależność między temperaturą a PM10, jednak nie jest tak znacząca. Im temperatura jest niższa, tym powietrze staje się bardziej zanieczyszczone.
+          Połączenie to jest spowodowane spalaniem większej ilości paliw w celu ogrzania mieszkań, biur czy fabryk.
+          <br></br><br></br>
+          
+          Równie znaczącą relacją jest to, że wszystkie zanieczyszczenia występują odwrotnie proporcjonalnie do prędkości wiatru, który im z większą prędkością wieje, tym bardziej rozwiewa zawieszone w nim cząsteczki.
+          <br></br>
+          Nasze obliczenia wskazały brak jednoznacznego powiązania między obecnością szkodliwych substancji w powietrzu a zachmurzeniem.
+          <br></br><br></br>
+          Podsumowując najlepszą porą do spędzania czasu na dworze są ciepłe i wietrzne dni. :)
+        </div>
       </div>
-      
     </div>
   );
 }
--- air-quality-app/client/src/components/MapSelector.tsx
+++ air-quality-app/client/src/components/MapSelector.tsx
@@ -1,25 +1,25 @@
 //@ts-nocheck
 //there are some properties in jsx that ts does not particularly like
-import React from 'react';
-import {render} from 'react-dom';
+import React, {useState} from 'react';
 import StaticMap from 'react-map-gl';
 import DeckGL from '@deck.gl/react/typed';
 import {HeatmapLayer} from '@deck.gl/aggregation-layers/typed';
+import { Calendar } from "react-calendar";
+import Select from "react-select";
+import { getMapData } from "../api/api";
 import '../styles/Map.css'
 import 'mapbox-gl/dist/mapbox-gl.css';
 
-const DATA_URL =
-  'https://raw.githubusercontent.com/visgl/deck.gl-data/master/examples/screen-grid/uber-pickup-locations.json'; // eslint-disable-line
 
 const API_KEY = 'pk.eyJ1IjoibXJvY2hueSIsImEiOiJjbGJmY2hiamQwNTVoM3ZzMndzNGNodmN4In0.bSbi4473dvEs_RCJYHENZA' //please don't steal, here only for the convenience of grading
 
 type MapData = {coordinates: [number, number], value: number}[]
-const testData: MapData = [{coordinates: [18.493331, 54.560836], value: 1},
-                           {coordinates: [18.464911, 54.465758], value: 1},
-                           {coordinates: [18.57884, 54.43451], value: 1},
-                           {coordinates: [18.657497, 54.400833], value: 1},
-                           {coordinates: [18.620274, 54.380279], value: 2},
-                           {coordinates: [18.635283, 54.353336], value: 1},
+const testData: MapData = [{coordinates: [18.493331, 54.560836], value: 0},
+                           {coordinates: [18.464911, 54.465758], value: 0},
+                           {coordinates: [18.57884, 54.43451], value: 0},
+                           {coordinates: [18.657497, 54.400833], value: 0},
+                           {coordinates: [18.620274, 54.380279], value: 0},
+                           {coordinates: [18.635283, 54.353336], value: 0},
                         ];
 
 const INITIAL_VIEW_STATE = {
@@ -34,12 +34,40 @@ const INITIAL_VIEW_STATE = {
 const MAP_STYLE = 'mapbox://styles/mapbox/streets-v12';
     
 export function MapSelector({
-    data = testData,
     intensity = 1,
     threshold = 0.03,
     radiusPixels = 200,
     mapStyle = MAP_STYLE
     }) {
+
+    //possible pollution types
+    const options = [
+        { value: 'co2', label: 'Carbon Monoxide' },
+        { value: 'no2', label: 'Nitrogen Dioxide' },
+        { value: 'pm10', label: 'PM10' },
+    ];
+    
+    let [isLoading, setIsLoading] = useState(false);
+    //chosen time
+    let [day, setDay] = useState(new Date(2021, 0, 1));
+    //data to be displayed on the map
+    let [data, setData] = useState(testData)
+    //chosen pollution type
+    let [pollution, setPollution] = useState(options[0])
+
+    const handleClick = () => {
+        setIsLoading(true)
+        getMapData().then((response) => {
+            let responseData = JSON.parse(response);
+            setData(responseData)
+            setIsLoading(false)
+        })
+    }
+
+    const handleChange = (value: Date) => {
+        setDay(value);
+    }
+
     const layers = [
         new HeatmapLayer({
         data,
@@ -54,11 +82,38 @@ export function MapSelector({
         })
     ];
     
+
+    //rendering logic
     return (
-        <div style={{position:'relative', width: 600, height: 400, margin: '2rem'}}>
-            <DeckGL initialViewState={INITIAL_VIEW_STATE} controller={true} layers={layers}>
-            <StaticMap reuseMaps mapStyle={mapStyle} preventStyleDiffing={true} mapboxAccessToken={API_KEY}/>
-            </DeckGL>
-        </div> 
+        <>
+            <div className='MapParams'>
+                <Calendar 
+                    onChange={handleChange} 
+                    value={day} 
+                    minDate={new Date(2021, 0, 1)} 
+                    maxDate={new Date(2021, 11, 31)} 
+                    defaultActiveStartDate={new Date(2021, 0, 1)} 
+                    next2Label={null} 
+                    prev2Label={null}
+                    minDetail={'year'}
+                />
+                <div style={{color: 'black'}}>
+                    <Select 
+                        defaultValue={pollution}
+                        onChange={setPollution}
+                        options={options}
+                        className="react-select-container"
+                        classNamePrefix="react-select" 
+                    />
+                </div>
+                {isLoading ? <div className='Loader'></div> : <button className="FetchButton" onClick={handleClick}>Check the pollution!</button>}
+            </div>
+            <div style={{position:'relative', width: '50%', height: '50vh', margin: '2rem'}}>
+                <DeckGL initialViewState={INITIAL_VIEW_STATE} controller={true} layers={layers}>
+                <StaticMap reuseMaps mapStyle={mapStyle} mapboxAccessToken={API_KEY}/>
+                </DeckGL>
+            </div> 
+        </>
+        
     );
     }
