bb098b37025d3eb8a3cc8d6c9b942fbf0a87cccf
Mateusz Naklicki <mateusz.naklicki06@gmail.com>
2022-12-09 00:36:38 +0100

    Format analysis section and import images

---
+++ air-quality-app/client/src/img/analyzeImages.ts
@@ -0,0 +1,15 @@
+import co_t from './co_t.png';
+import co_w from './co_w.png';
+import no_t from './no_t.png';
+import no_w from './no_w.png';
+import pm_t from './pm_t.png';
+import pm_w from './pm_w.png';
+
+export default [
+    co_t,
+    co_w,
+    no_t,
+    no_w,
+    pm_t,
+    pm_w
+]
